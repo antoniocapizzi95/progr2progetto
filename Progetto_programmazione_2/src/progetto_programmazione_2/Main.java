ae4e6e454ff63e47d95fee297d7cffbfd6b9dee7
Antonio <ac2@nigithorconsulting.com>
2017-06-05 15:05:26 +0200

    implementazione prime classi e gui

--- Progetto_programmazione_2/src/progetto_programmazione_2/Main.java
+++ Progetto_programmazione_2/src/progetto_programmazione_2/Main.java
@@ -16,6 +16,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GUI frame = new GUI();
+        frame.setVisible(true);
     }
     
 }
